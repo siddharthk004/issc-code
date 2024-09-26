594cf6951dcc221214abb62b8539fb1dcd3fa59f
kardile siddharth satish <127118293+siddharthk004@users.noreply.github.com>
2024-09-26 18:53:36 +0530

    Add files via upload

---
+++ code-practice/program-java/git usage.c
@@ -0,0 +1,45 @@
+/*
+//////////////////////////////////////////////////////////////////////
+
+            upload file using git on github when repo is not add or init
+
+            1 - git init
+            2 - git add README.md
+            3 - git commit -m "first commit"
+            4 - git branch -M main
+            5 - git remote add origin https://github.com/siddharthk004/rwf.git
+            6 - git push -u origin main
+
+/////////////////////////////////////////////////////////////////////
+
+            upload file using git on github when repo is already added or init
+            
+            1 - git remote add origin https://github.com/siddharthk004/rwf.git
+            2 - git branch -M main
+            3 - git push -u origin main
+ 
+ //////////////////////////////////////////////////////////////////////
+
+            hosting a react application
+
+            1 - on browser create react app type and use that commands
+            2 - github pages  [.github.io]
+            3 - 
+
+
+///////////////////////////////////////////////////////////////////////////
+
+*/
+
+
+// PrayDev – A company centered around software or web development.
+
+
+
+// PrayDev can be interpreted as a combination of two parts:
+
+// Pray: In this context, "Pray" could symbolize a sense of purpose, dedication, or focus, potentially suggesting a company that is committed to delivering thoughtful and impactful solutions.
+// Dev: Short for "Development," typically referring to software development, web development, or technology innovation.
+// Together, PrayDev could represent a company that is devoted to developing high-quality software or technical solutions with a focus on thoughtful and purposeful work. The name conveys a sense of dedication to innovation and development.
+
+///////////////////////////////////////////////////////////////////////////
